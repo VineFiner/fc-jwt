762072f39c2bfc2612666c2aae26fb4785adfa5f
Vine <2282458988@qq.com>
2023-03-18 22:10:15 +0800

    chore: change readme

--- scripts/build_ubuntu_focal.sh
+++ scripts/build_ubuntu_focal.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 # 构建镜像，指定 Dockerfile 文件路径 `./Dockerfile`, 指定构建上下文目录 `./`
-docker build --target build -t vapor_app_ubuntu -f ./Dockerfile ./
+docker build -t vapor_app_ubuntu -f ./Dockerfile ./
 
 # 复制构建产物
-#docker create --name extract vapor_app_ubuntu
-#docker cp extract:/app pkg
-#chmod -R 777 pkg
-#docker rm -f extract
-
 docker run --rm  \
 -w /workspace \
 -v $(pwd):/workspace \
+--user=root \
+--entrypoint /bin/bash \
 vapor_app_ubuntu \
-bash -c "\
+-c "\
     rm -rf pkg \
-    && cp -r /staging pkg \
+    && cp -r /app pkg \
     && chmod -R 777 pkg"
     
 # 添加启动文件
@@ -27,4 +24,6 @@ cat > ./pkg/scf_bootstrap<<EOF
 EOF
 
 # 打包 资源目录 `./app`, 资源子目录 `./`
-tar cvzf vapor_app-ubuntu.zip -C ./pkg .
+# tar cvzf vapor_app-ubuntu.zip -C ./pkg .
+
+cd pkg && tar cvzf vapor_app-ubuntu.zip *
